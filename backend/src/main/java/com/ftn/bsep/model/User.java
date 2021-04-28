be6821ab2846c1c96be7c0f573916af1a36c00c4
Milan Todorović <todorovic.ra11.2011@uns.ac.rs>
2021-04-28 19:25:10 +0200

    Popravljena serijalizacija

--- backend/src/main/java/com/ftn/bsep/model/User.java
+++ backend/src/main/java/com/ftn/bsep/model/User.java
@@ -1,9 +1,11 @@
 package com.ftn.bsep.model;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 @Entity
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
